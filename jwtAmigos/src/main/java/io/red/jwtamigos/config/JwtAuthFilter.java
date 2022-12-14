752547b7033b9966b9f8d958e1c7c71c7f5701f6
Erasmo Jr <erasmo.cjunior@gmail.com>
2022-12-14 17:02:27 -0300

    :recycle: REFACT: (JwtAuthFilter) Metodo de autenticacao finalizado

--- jwtAmigos/src/main/java/io/red/jwtamigos/config/JwtAuthFilter.java
+++ jwtAmigos/src/main/java/io/red/jwtamigos/config/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -48,6 +49,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                 userDetails,
                                 null,
                                 userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
         filterChain.doFilter(request, response);
