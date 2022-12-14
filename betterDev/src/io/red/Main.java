1595d5c2db820df53158c1ec9663e0f92881c889
Erasmo Jr <erasmo.junior@gmail.com>
2022-12-14 19:16:10 -0300

    :heavy_plus_sign: BUILD: Adicionado e atualizando projetos

--- betterDev/src/io/red/Main.java
+++ betterDev/src/io/red/Main.java
@@ -1,4 +1,9 @@
 package io.red;
 
 public class Main {
+    public static void main(String[] args) {
+        boolean yes = false;
+
+
+    }
 }
--- betterDev/src/io/red/capitulo3/MenosCodigo.java
+++ betterDev/src/io/red/capitulo3/MenosCodigo.java
@@ -1,2 +1,18 @@
-package io.red.capitulo3;public class MenosCodigo {
+package io.red.capitulo3;
+
+public class MenosCodigo {
+
+    // Logica oscilante
+    boolean yes = true;
+    
+            if(yes)
+
+    {
+        return true;
+    } else
+
+    {
+        return false;
+    }
+
 }
